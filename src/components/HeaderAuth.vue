b75728056b6ae73bf9f67e2d1e54511450164e2f
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-07 19:24:26 +0900

    修正内容が分かるコミットメッセージ

--- src/components/HeaderAuth.vue
+++ src/components/HeaderAuth.vue
@@ -1,6 +1,10 @@
 <template>
  <div class="header flex">
-   <div class="flex">
+   <div></div>
+   <div class="center">
+     <h1>付箋ボード</h1>
+   </div>
+   <div class="center flex">
      <p @click="$router.push('/signup')">新規登録</p>
      <p>|</p>
      <p @click="$router.push('/')">ログイン</p>
@@ -9,18 +13,25 @@
 </template>
 
 <style scoped>
-
-.header {
- margin: 20px;
-  text-align:center;
+h1{
+  color:rgb(92, 69, 7);
+  font-size:36px;
+  text-shadow:2px 2px 2px black;
+  padding:10px;
 }
-.logo {
- width: 150px;
+.center{
+  cursor: pointer;
+}
+.right p {
+ margin-right: 20px;
  cursor: pointer;
 }
+.header {
+ margin: 20px;
+}
+
 .flex {
  display: flex;
  justify-content: space-between;
-
 }
 </style>
